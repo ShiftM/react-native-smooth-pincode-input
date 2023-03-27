3769caecbe54e23deedcda372a174b21dcf1651b
Luis Ramirez <emmanuelluis.ramirez@gmail.com>
2023-03-28 04:11:28 +0800

    Removed Proptypes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import {
   TextInput,
   StyleSheet,
   I18nManager,
-  ViewPropTypes,
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import ViewPropTypes from 'deprecated-react-native-prop-types/DeprecatedViewPropTypes';
+import TextPropTypes from 'deprecated-react-native-prop-types/DeprecatedTextPropTypes';
 
 const styles = StyleSheet.create({
   containerDefault: {},
@@ -296,9 +297,9 @@ SmoothPinCodeInput.propTypes = {
   cellStyleFocused: ViewPropTypes.style,
   cellStyleFilled: ViewPropTypes.style,
 
-  textStyle: Text.propTypes.style,
-  textStyleFocused: Text.propTypes.style,
-
+  textStyle: TextPropTypes.style,
+  textStyleFocused: TextPropTypes.style,
+  
   animated: PropTypes.bool,
   animationFocused: PropTypes.oneOfType([
     PropTypes.string,
